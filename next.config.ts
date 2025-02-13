cfcd83a243d13a2ebd72d5762cbf65d3b8fd445c
Integral-07 <mnt.0710@docomo.ne.jp>
2025-02-13 06:43:45 +0000

    login function

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,15 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+
+  async rewrites(){
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://host.docker.internal:8000/api/:path*/",
+      }
+    ]
+  },
+};
